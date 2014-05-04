dc374e0783e28700c97f53de5e6efde3b4ce5277
Sitati Kituyi <sitati.kituyi@googlemail.com>
2014-05-04 17:10:03 +0300

    Added more drawables

--- sokoban.js
+++ sokoban.js
@@ -10,12 +10,26 @@ Sokoban = function() {
 	init = function() {
 		sokoban.constants = new Constants(10, 600);
 		drawGridLines();
+
 		var b = new sokoban.drawable.DeadBlock();
 		b.putAt(3,2);
 		b.draw();
+
 		var p = new sokoban.drawable.Player();
 		p.putAt(8,1);
 		p.draw();
+
+		var t = new sokoban.drawable.Target(3);
+		t.putAt(9,3);
+		t.draw();
+
+		var m = new sokoban.drawable.MovableBlock(2);
+		m.putAt(3,8);
+		m.draw();
+
+		var plus = new sokoban.drawable.MathBlock('+', function() { console.log('plusss'); });
+		plus.putAt(5,3);
+		plus.draw();
 	},
 	drawGridLines = function() {
 		var gradient=context.createLinearGradient(0,0,600, 600);
@@ -72,8 +86,98 @@ Sokoban = function() {
 			context.fill();
 		};
 	};
+	sokoban.drawable.Target = function(val) {
+		this.value = val;
+		this.draw = function() {
+			console.log('draw target');
+			var gradient=context.createLinearGradient(0,0,600, 600);
+			gradient.addColorStop("0","#794");
+			gradient.addColorStop("1","#794");
+			context.strokeStyle=gradient;
+			context.lineWidth=3;
+			context.beginPath();
+			context.moveTo(this.pX, this.pY);
+			context.lineTo(this.pX + sokoban.constants.blockWidth, this.pY);
+			context.lineTo(this.pX + sokoban.constants.blockWidth, this.pY + sokoban.constants.blockWidth);
+			context.lineTo(this.pX, this.pY + sokoban.constants.blockWidth);
+			context.lineTo(this.pX, this.pY);
+			context.stroke();
+			context.closePath();
+			context.beginPath();
+			context.lineWidth=1.5;
+			context.font="60px Sans-Serif";
+			context.fillStyle = gradient;
+			context.textAlign = 'center';
+			context.textBaseline = 'middle';
+			context.strokeText(this.value, this.pX + (sokoban.constants.blockWidth / 2), this.pY + (sokoban.constants.blockWidth / 2));
+			context.closePath();
+		};
+	};
+	sokoban.drawable.MovableBlock = function(val) {
+		this.value = val;
+		this.draw = function() {
+			console.log('draw movable');
+			var gradient=context.createLinearGradient(0,0,600, 600);
+			gradient.addColorStop("0","#794");
+			gradient.addColorStop("1","#794");
+			context.fillStyle=gradient;
+			context.lineWidth=3;
+			context.beginPath();
+			context.moveTo(this.pX, this.pY);
+			context.lineTo(this.pX + sokoban.constants.blockWidth, this.pY);
+			context.lineTo(this.pX + sokoban.constants.blockWidth, this.pY + sokoban.constants.blockWidth);
+			context.lineTo(this.pX, this.pY + sokoban.constants.blockWidth);
+			context.lineTo(this.pX, this.pY);
+			context.fill();
+			context.closePath();
+			gradient.addColorStop("0","#fff");
+			gradient.addColorStop("1","#fff");
+			context.beginPath();
+			context.lineWidth=1.5;
+			context.font="60px Sans-Serif";
+			context.fillStyle = gradient;
+			context.textAlign = 'center';
+			context.textBaseline = 'middle';
+			context.fillText(this.value, this.pX + (sokoban.constants.blockWidth / 2), this.pY + (sokoban.constants.blockWidth / 2));
+			context.closePath();
+		};
+	};
+	sokoban.drawable.MathBlock = function(symbol, applyOperand) {
+		this.symbol = symbol;
+		this.draw = function() {
+			console.log('draw movable');
+			var gradient=context.createLinearGradient(0,0,600, 600);
+			gradient.addColorStop("0","#bbd");
+			gradient.addColorStop("1","#bbd");
+			context.strokeStyle=gradient;
+			context.fillStyle=gradient;
+			context.lineWidth=3;
+			context.beginPath();
+			context.moveTo(this.pX, this.pY);
+			context.lineTo(this.pX + sokoban.constants.blockWidth, this.pY);
+			context.lineTo(this.pX + sokoban.constants.blockWidth, this.pY + sokoban.constants.blockWidth);
+			context.lineTo(this.pX, this.pY + sokoban.constants.blockWidth);
+			context.lineTo(this.pX, this.pY);
+			context.fill();
+			context.closePath();
+			gradient=context.createLinearGradient(0,0,600, 600);
+			gradient.addColorStop("0","#fff");
+			gradient.addColorStop("1","#fff");
+			context.beginPath();
+			context.lineWidth=1.5;
+			context.font="60px Sans-Serif";
+			context.fillStyle = gradient;
+			context.textAlign = 'center';
+			context.textBaseline = 'middle';
+			context.fillText(this.symbol, this.pX + (sokoban.constants.blockWidth / 2), this.pY + (sokoban.constants.blockWidth / 2));
+			context.closePath();
+		};
+	};
 	sokoban.drawable.DeadBlock.prototype = Drawable.prototype;
 	sokoban.drawable.Player.prototype = Drawable.prototype;
+	sokoban.drawable.Target.prototype = Drawable.prototype;
+	sokoban.drawable.MovableBlock.prototype = Drawable.prototype;
+	sokoban.drawable.MathBlock.prototype = Drawable.prototype;
 	init();
 },
 Drawable = function() { };
